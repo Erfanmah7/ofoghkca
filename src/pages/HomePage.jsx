3d691dfd21d31b6bb573dc8ac94994b49dca668e
ofoghkca <ofoghkavirardakan@gmail.com>
2024-08-24 15:17:06 +0330

    card box

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,6 @@ import React from "react";
 function HomePage() {
   return (
     <div className="mt-[246px] flex flex-col justify-center md:container">
-
       {/* banner */}
       <div
         href="#"
@@ -29,7 +28,7 @@ function HomePage() {
         </div>
       </div>
 
-{/* boxs */}
+      {/* boxs */}
 
       <div className="grid grid-cols-3 mt-10 gap-x-[33px] text-xl font-DanaMedium">
         <div className="rounded-[20px]  bg-violet-100  h-[99px] flex justify-between items-center px-8">
@@ -69,15 +68,57 @@ function HomePage() {
         </div>
       </div>
 
+      {/* box card */}
 
-{/* box card */}
+      <div className="w-full relative h-[389px] rounded-[25px] text-white bg-violet-700 mt-[41px] mb-[264px]">
+        <h3 className="mt-[90px] text-2xl mb-8 font-DanaMedium">
+          آژانس دیجیتال مارکتینگ دیمالند، چگونه به توسعه کسب و کار شما کمک می
+          کند؟
+        </h3>
+        <div className="flex justify-center items-center mb-[75px]">
+          <span className="bg-orange-300 h-1 w-[150px] flex rounded-full"></span>
+        </div>
 
-<div className="w-full h-[541px] rounded-[25px] bg-violet-300 mt-20">
+        <div className="flex absolute -bottom-25 right-0 left-0 justify-center mx-24 gap-x-[43px] ">
+          <div className="bg-white text-black w-[320px] h-[304px] rounded-[18px] shadow-md flex flex-col items-center justify-center px-7 pt-[49px] pb-[54px]">
+            <div className="w-16 h-16 flex justify-center items-center rounded-full bg-gradient-to-t to-orange-300 from-white">
+              <svg className="z-30 h-10 w-10 translate-y-7">
+                <use href="#moon-icon"></use>
+              </svg>
+            </div>
+            <h3 className="mb-4 mt-[54px]">ارائه متدهای جدید کسب و کار</h3>
+            <p className="text-gray-400">
+              بررسی حوزه فعالیت و ارائه روش‌های نوین
+            </p>
+          </div>
 
-
-
-</div>
-
+          <div className="flex">
+            <div className="bg-white text-black z-30 w-[320px] h-[304px] rounded-[18px] shadow-md flex flex-col items-center justify-center px-7 pt-[49px] pb-[54px]">
+              <div className="w-16 h-16 flex justify-center items-center rounded-full bg-gradient-to-t to-orange-300 from-white">
+                <svg className="z-30 h-10 w-10 translate-y-7">
+                  <use href="#moon-icon"></use>
+                </svg>
+              </div>
+              <h3 className="mb-4 mt-[54px]">ارائه متدهای جدید کسب و کار</h3>
+              <p className="text-gray-400">
+                بررسی حوزه فعالیت و ارائه روش‌های نوین
+              </p>
+            </div>
+            <div className="z-20 bg-white/20 absolute -rotate-12 w-[320px] h-[304px] rounded-[18px]"></div>
+          </div>
+          <div className="bg-white text-black w-[320px] h-[304px] rounded-[18px] shadow-md flex flex-col items-center justify-center px-7 pt-[49px] pb-[54px]">
+            <div className="w-16 h-16 flex justify-center items-center rounded-full bg-gradient-to-t to-orange-300 from-white">
+              <svg className="z-30 h-10 w-10 translate-y-7">
+                <use href="#moon-icon"></use>
+              </svg>
+            </div>
+            <h3 className="mb-4 mt-[54px]">ارائه متدهای جدید کسب و کار</h3>
+            <p className="text-gray-400">
+              بررسی حوزه فعالیت و ارائه روش‌های نوین
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
